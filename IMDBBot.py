60c523df39397785b68b2424b4096e9e846dcd87
BurakUsta1 <burakusta1@proton.me>
2024-11-03 10:23:00 +0300

    First Commit

---
+++ IMDBBot.py
@@ -0,0 +1,102 @@
+import os,csv
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+
+class ımdb:
+    def __init__(self):
+        #Selnium to show only important alerts when running
+        self.options = Options()
+        self.options.add_argument("--log-level=3")
+        self.browser = webdriver.Edge(options=self.options)
+    
+    def singIn(self):
+        #Get the IMDb Ratings List address from the user and open them.
+        self.link= input("\n"+'\033[93m'+"Enter the full address of your ratings list:"+'\033[97m')
+        self.browser.get(self.link)
+           
+    def MakeCSV(self,fileName):
+        #Save the CSV file to the location where IMDBBot runs.
+        columnTittle=["IMDb Rating","Title Type","Your Rating","Date Rated","Director","Release Year"]
+        self.fileName=fileName
+        print("\nCreating "+'\033[93m'+self.fileName+'\033[97m'+" file...\n")
+        self.script_directory = os.path.dirname(os.path.abspath(__file__))
+        self.csv_file_path = os.path.join(self.script_directory, self.fileName)
+        with open(self.csv_file_path, mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow(columnTittle)
+
+    def RatingList(self):
+
+        #Total number of ratings
+        RatingCount= WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.XPATH,"/html/body/div[2]/main/div/section/div/section/div/div[1]/section/div[1]/div[1]"))).text
+        print("\n"+RatingCount[:5]+"entry found.\n\nEntry are being prepared...")
+        lis = self.browser.find_elements(By.CLASS_NAME,"ipc-metadata-list-summary-item")
+        
+        #Pull the slider down until the entire rating list is created.
+        while int(RatingCount[:4]) != len (lis):
+            self.browser.execute_script("window.scrollTo(0, document.body.scrollHeight * 0.98);")
+            lis = self.browser.find_elements(By.CLASS_NAME,"ipc-metadata-list-summary-item")
+            continue
+        else:
+            #Create the csv file.
+            self.MakeCSV('Rating List.csv')
+
+            #Get the data
+            for i,item in enumerate(lis):
+                
+                progress = (i + 1) / len(lis)
+                barLength = 20
+                bar = "█" * int(progress * barLength) + "-" * (barLength - int(progress * barLength))
+                print('\033[93m' + f"\rLoading: [{bar}] %{int(progress * 100)}", end="")
+
+
+                try:
+                    Title = item.find_element(By.CLASS_NAME,"ipc-title__text").text
+                except:
+                    Title = " "
+                try:
+                    IMDbRating = item.find_element(By.CLASS_NAME, "ipc-rating-star--rating").text
+                except:
+                    IMDbRating=" "
+                try:
+                    YourRating = item.find_elements(By.CLASS_NAME, "ipc-rating-star--rating")[1].text
+                except:
+                    YourRating=" "
+                try:
+                    DateRated= item.find_element(By.CSS_SELECTOR, ".sc-5bc66c50-9.ekmeXq.dli-user-list-item-date-added").text
+                except:
+                    DateRated=" "
+                try:
+                    Director= item.find_element(By.CSS_SELECTOR,"span.sc-54004b59-5 a.ipc-link.ipc-link--base.dli-director-item").text
+                except:
+                    Director=" "
+                try:
+                    ReleaseYear=item.find_element(By.CLASS_NAME,"sc-5bc66c50-6.OOdsw.dli-title-metadata-item").text
+                except:
+                    ReleaseYear=" "
+                try:
+                    TitleType=item.find_element(By.CLASS_NAME,"sc-5bc66c50-3.WQtNy.dli-title-type-data").text
+                except:
+                    TitleType="Movie"
+                try:
+                    Link= item.find_element(By.CLASS_NAME,"ipc-title-link-wrapper").get_attribute("href")
+                except:
+                    Link=" "
+
+                #Write the data to CSV file
+                try:   
+                    row= [Title,TitleType,IMDbRating,YourRating,DateRated[9:],Director,ReleaseYear,Link]
+                    with open(self.csv_file_path, mode='a', newline='', encoding='utf-8') as file:
+                        writer = csv.writer(file)
+                        writer.writerow(row)
+                except Exception as e:
+                    print("HATA",e)
+            print ("\n\n"+'\033[97m'+str(len(lis))+" lines successfully written to "+'\033[93m'+self.fileName+'\033[97m'+" file.\n\nThe file is saved to "+'\033[93m'+self.script_directory+'\033[97m'+" location." )
+ımdb=ımdb()
+ımdb.singIn()
+ımdb.RatingList()
